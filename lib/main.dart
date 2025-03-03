4af2f383d4dae6d4677f78f29be54bda6f9f1edb
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-03-03 09:42:41 +0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'ListView/Components/ScrollableList.dart';
+import 'ListView/ContectNumber/CardListView.dart';
+import 'ListView/ContectNumber/Animatedpage.dart';
+import 'ListView/Masjid.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
 
       ),
-      home: Scrollablelist(),
+      home: Animatedpage(),
 
     );
   }
