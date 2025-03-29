8c887a33354be49f5f34de0c3b2f8bed7e7a312c
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-03-29 21:17:19 +0500

    update

--- lib/SliverWigets/SliverList/listSliver.dart
+++ lib/SliverWigets/SliverList/listSliver.dart
@@ -18,15 +18,14 @@ class _listSliverState extends State<listSliver> {
             floating: false,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text('SliverList Example'),
-              background: Container(
-                color: Colors.blue,
-              )
-            ),
+                title: Text('SliverList Example'),
+                background: Container(
+                  color: Colors.blue,
+                )),
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (context, index) => ListTile(
+              (context, index) => ListTile(
                 title: Text('Item $index'),
                 subtitle: Text('Subtitle for item $index'),
                 leading: CircleAvatar(child: Text('$index')),
@@ -34,6 +33,31 @@ class _listSliverState extends State<listSliver> {
               childCount: 20, // Number of items
             ),
           ),
+          SliverToBoxAdapter(
+            child: Center(
+              child: Text("Slver peding",style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 33
+
+              ),),
+            ),
+          ),
+          SliverPadding(
+              padding:
+                  EdgeInsets.all(16.0), // Add padding around the SliverList
+              sliver: SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) => Card(
+                    child: ListTile(
+                      title: Text('Item $index'),
+                      subtitle: Text('Subtitle for item $index'),
+                      leading: CircleAvatar(child: Text('$index')),
+                    ),
+                  ),
+                  childCount: 15, // Number of items
+                ),
+              ))
         ],
       ),
     );
